ff0b4076b9bbe9cdc689de4d6b74171fd865d6d7
amac81 <arnaldocanelas@gmail.com>
2024-07-02 15:42:27 +0100

    Program update

--- src/app/Program.java
+++ src/app/Program.java
@@ -4,6 +4,8 @@ import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.HashMap;
+import java.util.Map;
 
 import db.DB;
 import entities.Order;
@@ -18,20 +20,42 @@ public class Program {
 	
 		Statement st = conn.createStatement();
 			
-		ResultSet rs = st.executeQuery("select * from tb_order");
+		ResultSet rs = st.executeQuery("SELECT * FROM tb_order "
+				+ "INNER JOIN tb_order_product ON tb_order.id = tb_order_product.order_id "
+				+ "INNER JOIN tb_product ON tb_product.id = tb_order_product.product_id");
+		
+		
+		Map<Long, Order> ordersMap = new HashMap <> ();
+		Map<Long, Product> productsMap = new HashMap <> ();
 		
 		while (rs.next()) {
+			Long orderId = rs.getLong("order_id");
 			
-			Order o = instatiateOrder(rs); 
-			//System.out.println(rs.getLong("Id") + ", " + rs.getString("Name"));
-			System.out.println(o);
+			if(ordersMap.get(orderId) == null) {
+				Order order = instatiateOrder(rs); 
+				ordersMap.put(orderId, order);
+			}
+					
+			Long productId = rs.getLong("product_id");
+			
+			if(productsMap.get(productId) == null) {
+				Product product = instatiateProduct(rs); 
+				productsMap.put(productId, product);
+			}
+			
+			ordersMap.get(orderId).getProducts().add(productsMap.get(productId));
+		}
+		
+		
+		for(Long orderId : ordersMap.keySet()) {
+			System.out.println(ordersMap.get(orderId));
 			
 		}
 	}
 	
 	private static Product instatiateProduct(ResultSet rs) throws SQLException {
 		Product p = new Product();
-		p.setId(rs.getLong("id"));
+		p.setId(rs.getLong("product_id"));
 		p.setName(rs.getString("name"));
 		p.setDescription(rs.getString("description"));
 		p.setImageUri(rs.getString("image_uri"));
@@ -42,7 +66,7 @@ public class Program {
 	
 	private static Order instatiateOrder(ResultSet rs) throws SQLException {
 		Order o = new Order();
-		o.setId(rs.getLong("id"));
+		o.setId(rs.getLong("order_id"));
 		o.setLatitude(rs.getDouble("latitude"));
 		o.setLongitude(rs.getDouble("longitude"));
 		o.setMoment(rs.getTimestamp("moment").toInstant());
