bc9165986a0f16f25e0adee05e59925dce06ccc1
amac81 <arnaldocanelas@gmail.com>
2024-07-02 14:13:46 +0100

    instatiateProduct create

--- src/app/Program.java
+++ src/app/Program.java
@@ -6,6 +6,7 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 import db.DB;
+import entities.Product;
 
 public class Program {
 
@@ -16,9 +17,24 @@ public class Program {
 		Statement st = conn.createStatement();
 			
 		ResultSet rs = st.executeQuery("select * from tb_product");
-			
+		
 		while (rs.next()) {
-			System.out.println(rs.getLong("Id") + ", " + rs.getString("Name"));
+			
+			Product p = instatiateProduct(rs); 
+			//System.out.println(rs.getLong("Id") + ", " + rs.getString("Name"));
+			System.out.println(p);
+			
 		}
+	}
+	
+	private static Product instatiateProduct(ResultSet rs) throws SQLException {
+		Product p = new Product();
+		p.setId(rs.getLong("id"));
+		p.setName(rs.getString("name"));
+		p.setDescription(rs.getString("description"));
+		p.setImageUri(rs.getString("image_uri"));
+		p.setPrice(rs.getDouble("price"));
+		
+		return p;		
 	}
 }
